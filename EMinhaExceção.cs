f54bc24a8783a88050aa61790c43b5ee90436f4e
Merizito <30642904+Merizito@users.noreply.github.com>
2017-11-11 10:25:52 -0300

    Add files via upload

---
+++ EMinhaExceção.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tratamento_de_Exceções___pt._2
+{
+    class EMinhaExceção : Exception
+    {
+        int numero = 0, soma = 0;
+        public EMinhaExceção(string msg)
+        {
+
+        }
+
+        public static int Somatorio(int _num, int _soma)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Clear();
+                Console.Write("Digite um número: ");
+                _num = int.Parse(Console.ReadLine());
+                _soma = _soma + _num;
+            }
+            return _soma;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tratamento_de_Exceções___pt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0, num = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                EMinhaExceção.Somatorio(num, soma);
+            }
+            soma = soma / 100;
+            //throw new EMinhaExceção("");
+
+        }
+    }
+}
